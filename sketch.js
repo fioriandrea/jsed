86fcf9dbacc2a57529dba1b59bf5f06d443a95e7
Andrea Fiori <andrea.fiori.1998@gmail.com>
2020-10-22 14:31:34 +0200

    put lower cap to witdth and height

--- sketch.js
+++ sketch.js
@@ -10,21 +10,24 @@ let keyRecorder;
 
 let hrcanvas;
 
+const width = () => Math.max(100, getWindowWidth());
+const height = () => Math.max(100, getWindowHeight());
+
 function updateScreenData() {
     screenService.adjustScreen(editor);
 }
 
 function windowResized() {
-    hrcanvas.setSize(getWindowWidth(), getWindowHeight());
-    screen.width = getWindowWidth();
-    screen.height = getWindowHeight();
+    hrcanvas.setSize(width(), height());
+    screen.width = width();
+    screen.height = height();
     updateScreenData();
     screenService.adjustScreenLines();
     drawEditor();
 }
 
 function setup() {
-    hrcanvas = new HRCanvas(appendCanvas(), getWindowWidth(), getWindowHeight());
+    hrcanvas = new HRCanvas(appendCanvas(), width(), height());
 
     lines = new Lines();
     cursor = new Cursor(lines, 0, 0);
